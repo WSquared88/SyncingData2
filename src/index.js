b6fef099326c75247656f9654a0bfe58bd78484c
Westley Waligora (RIT Student) <wsw3877@ad.rit.edu>
2016-03-07 22:06:13 -0500

    Added the flag and collision detection. Players now get points for collecting the flag

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ app.listen(PORT);
 
 var squares = {};
 
+var flag = 
+{
+	x: Math.random()*400,
+    y: Math.random()*400,
+	radius: 10,
+	color: "purple"
+};
+
 function handler(req, res)
 {
 	fs.readFile(__dirname + "/../client/client.html", function(err, data)
@@ -21,17 +29,61 @@ function handler(req, res)
 	});
 }
 
+function checkCollision(id)
+{
+	//if(Math.abs(squares[id].x - flag.x) > flag.radius + squares[id].width/2)
+	//{
+	//	return;
+	//}
+	
+	if(Math.abs(squares[id].x + squares[id].width/2 - flag.x) > flag.radius + squares[id].width/2)
+	{
+		return;
+	}
+	
+	//if(Math.abs(squares[id].y - flag.y) > flag.radius + squares[id].height/2)
+	//{
+	//	return;
+	//}
+	
+	if(Math.abs(squares[id].y + squares[id].height/2 - flag.y) > flag.radius + squares[id].height/2)
+	{
+		return;
+	}
+	
+	squares[id].points += 1;
+	
+	var message = 
+	{
+		message: "",
+		data: squares,
+		flag: flag
+	};
+	
+	moveFlag();
+	
+	io.sockets.in("room1").emit("updatePoints", message);
+}
+
+function moveFlag()
+{
+	flag.x = Math.random()*400;
+	flag.y = Math.random()*400;
+}
+
 io.on("connection", function(socket)
 {
 	socket.join("room1");
 	
 	socket.on("init", function(data)
 	{
+		console.log(flag);
 		squares[data.id] = data.data;
 		var message = 
 		{
 			message: "",
-			data: squares
+			data: squares,
+			flag: flag
 		};
 		
 		io.sockets.in("room1").emit("allSquares", message);
@@ -43,13 +95,24 @@ io.on("connection", function(socket)
 		{
 			squares[data.id] = data.data;
 			
+			checkCollision(data.id);
+			
 			var message = 
 			{
 				message: "",
-				data: squares
+				data: squares,
+				flag: flag
 			};
 		
 			io.sockets.in("room1").emit("allSquares", message);
+		}
+	});
+	
+	socket.on("updateScore", function(data)
+	{
+		if( data.score > points)
+		{
+			points = data.score;
 		}
 	});
 	
@@ -60,7 +123,8 @@ io.on("connection", function(socket)
 		var message = 
 		{
 			message: "",
-			data: squares
+			data: squares,
+			flag: flag
 		};
 		
 		io.sockets.in("room1").emit("allSquares", message);
@@ -68,6 +132,7 @@ io.on("connection", function(socket)
 	
 	socket.on("disconnect", function(data)
 	{
+		console.log(data);
 		socket.leave("room1");
 	});
 });
